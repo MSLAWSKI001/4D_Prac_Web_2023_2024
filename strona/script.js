ad9da81abbef9e9fb140f6aad1880092db5a4396
MSLAWSKI001 <113010458+MSLAWSKI001@users.noreply.github.com>
2024-04-26 09:54:35 +0200

    Add files via upload

--- strona/script.js
+++ strona/script.js
@@ -4,7 +4,9 @@ var dammage = 1;
 function Atack() {
     document.getElementById("hp").innerHTML = entity_hp;
     document.getElementById("gold_txt").innerHTML = gold;
+    document.getElementById("entityHealth").innerHTML = entity_hp;
     entity_hp-=dammage;
+    document.getElementById("entityHealth").value = entity_hp;
     if (entity_hp < 0) {
         entity_hp = 19;
         gold = gold + 2;
